4c82b121bb4393d1ae8157d4dc663f1dd7943408
wphyo3820 <wphyo3820@gmail.com>
2018-12-21 20:53:09 +0000

    updated app to serve local data and for aws beanstalk hosting

--- rank.py
+++ rank.py
@@ -1,27 +1,24 @@
 import pandas as pd
 import numpy as np
-from nba_api.stats.endpoints import leaguedashplayerstats
 from sklearn import preprocessing
 
-def get_league_stats(last_n = 0):
-    params = {} if last_n == 0 else {"last_n_games": last_n}
-    league_stats = leaguedashplayerstats.LeagueDashPlayerStats(**params).get_data_frames()[0]
-    per_cols = ["REB", "AST", "TOV", "STL", "BLK", "PTS"]
-    league_stats[per_cols] = league_stats[per_cols].div(league_stats["GP"].values, axis = 0)
-    return league_stats
+default_cols = ["FGP", "FTP", "3PM", "PPG", "RPG", "APG", "SPG", "BPG", "TPG"]
 
-def get_rankings(league_stats, col_names: [str], n_results = 100):
+def get_rankings(col_names: [str], n_results: int) -> pd.DataFrame:
+    data = pd.read_csv("data.csv")
+    if col_names == []:
+        col_names = default_cols
+    print(col_names)
     scaler = preprocessing.StandardScaler()
-
-    scaled_league_stats = scaler.fit_transform(league_stats[col_names])
+    scaled_league_stats = scaler.fit_transform(data[col_names])
     scaled_league_stats = pd.DataFrame(scaled_league_stats, columns=col_names)
 
-    if "TOV" in col_names:
-        scaled_league_stats["TOV"] = scaled_league_stats["TOV"] * -1
+    if "TPG" in col_names:
+        scaled_league_stats["TPG"] = scaled_league_stats["TPG"] * -1
 
     scaled_league_stats["TOTAL"] = np.sum(scaled_league_stats.loc[:, col_names], axis = 1)
     scaled_league_stats = scaled_league_stats.round(3)
-    scaled_league_stats["PLAYER_NAME"] = league_stats["PLAYER_NAME"]
+    scaled_league_stats["NAME"] = data["NAME"]
     scaled_league_stats = scaled_league_stats.sort_values("TOTAL", ascending = False).head(n_results)
    
     ## add rank
